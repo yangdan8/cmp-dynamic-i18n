9e3932e3663403b56349e1b6590a2d7de0adb043
yd <y@d.com>
2019-11-17 10:49:11 +0800

    完成demo

--- src/cmp-bases/vue-base.ts
+++ src/cmp-bases/vue-base.ts
@@ -1,35 +1,32 @@
 import Vue from 'vue';
-import { Mixins } from 'vue-property-decorator';
+import { Mixins, Component } from 'vue-property-decorator';
 import VueI18n from 'vue-i18n';
 import { loadLangAsync } from '@/utils/lang';
 
+@Component
 export class VueBase extends Mixins(Vue) {
-  /**
-   * 国际化重写$t属性方法
-   */
-  public $t = function(
-    this: VueBase,
-    key: string,
-    locale?: VueI18n.Locale | VueI18n.Values,
-    values?: VueI18n.Values,
-  ) {
-    // tslint:disable-next-line:no-console
-    console.warn(9999334334);
-    // 之前的$t方法
-    const $t = Vue.prototype.$t as typeof VueI18n.prototype.t;
-    const { constructor } = this;
-    const cmpName = constructor.name;
-    const keyNew = `${cmpName}.${key}`;
-    if (typeof locale === 'object' && locale) {
-      return $t(keyNew, locale);
-    }
-    return $t(keyNew, locale!, values);
-  };
-
   /**
    * 修改组件语言
    */
   public loadLangAsync() {
     loadLangAsync.call(this);
+  }
+
+  public created() {
+    /**
+     * 国际化重写$t属性方法
+     */
+    this.$t = (
+      key: string,
+      locale?: VueI18n.Locale | VueI18n.Values,
+      values?: VueI18n.Values,
+    ) => {
+      // 之前的$t方法
+      const $t = Vue.prototype.$t as typeof VueI18n.prototype.t;
+      const { constructor } = this;
+      const cmpName = constructor.name;
+      const keyNew = `${cmpName}.${key}`;
+      return $t.call(this, keyNew, locale as any, values);
+    };
   }
 }
